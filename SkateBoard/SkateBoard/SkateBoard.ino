febf74f5969eb26b1597eb7d39d5099f1c4d8e21
Jinhyeok Nam <njh@tideinstitute.org>
2019-06-06 19:19:12 +0900

    style added

--- SkateBoard/SkateBoard/SkateBoard.ino
+++ SkateBoard/SkateBoard/SkateBoard.ino
@@ -5,50 +5,37 @@
  */
 
 #include "defconfig.h"
-#include "IntegralTrapezoidal.h"
 #include "KalmanFilter.h"
-#include "MovingAverageFilter.h"
 #include "MPU6050.h"
 #include "ComplementaryFilter.h"
 #include <Adafruit_NeoPixel.h>
-#include <math.h>
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
 
-#define SAMPLING_RATE	1 // 1ms
+#define SAMPLING_RATE	10 // 1ms
 
 #define NEOPIXEL_PIN	6 // D6
 #define NEOPIXEL_NUM	30 // LED SIZE 30
 
-
-
-double mFilteredAccelX;
 MPU6050 mMPU6050; // SDA:A4, SCL:A5
 ComplementaryFilter mComplementaryFilter(SAMPLING_RATE);
-MovingAverageFilter mMovingAverageFilter(10);
 KalmanFilter mKalmanFilter;
-IntegralTrapezoidal mIntegralTrapezoidal(SAMPLING_RATE);
 Adafruit_NeoPixel mNeoPixel(NEOPIXEL_NUM, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 uint32_t mColorValue;
 bool mColorDir;
 
-double SamplingAccel() {
+double Sampling() {
 	mMPU6050.implementation();
 	double ax, ay, az, gx, gy, gz;
-
 	mMPU6050.getDatas(&ax, &ay, &az, &gx, &gy, &gz, NULL);
 	mComplementaryFilter.implementation(ax, ay, az, gx, gy, gz);
-
-	//double accelYDeg = mComplementaryFilter.get(ACC_ANGLE_Y_DEG);
-	//double pitchDeg = mComplementaryFilter.get(PITCH_DEG);
 	double rollDeg = mComplementaryFilter.get(ROLL_DEG);
-
 	double filteredKAL = mKalmanFilter.filtering(rollDeg);
 	
-	Serial.println(fabs(filteredKAL));
-
-	return fabs(filteredKAL);
+	double ret = rollDeg;
+	Serial.println(ret);
+	return ret;
 }
 
 // the setup function runs once when you press reset or power the board
@@ -64,10 +51,11 @@ void setup() {
 // the loop function runs over and over again until power down or reset
 void loop() {
 	delay(SAMPLING_RATE);
-	mFilteredAccelX = SamplingAccel();
+	double mFilteredAccelX = Sampling();
 
 	for (int i = 0; i < NEOPIXEL_NUM; i++) {
 #if (NEOPIXEL_STYLE_1 == 1)
+		mFilteredAccelX = fabs(mFilteredAccelX);
 		if ((int)mFilteredAccelX < ACC_GYRO_MAX_VALUE / 3) {
 			if (i < ((int)(mFilteredAccelX) % (ACC_GYRO_MAX_VALUE / 3))) {
 				mNeoPixel.setPixelColor(i, mNeoPixel.Color(255, 255, 255));
@@ -92,13 +80,14 @@ void loop() {
 				mNeoPixel.setPixelColor(i, mNeoPixel.Color(0, 0, 0));
 			}
 		}
-		
 #elif (NEOPIXEL_STYLE_2 == 1)
-		double dim = 255.0 / ACC_GYRO_MAX;
+		mFilteredAccelX = fabs(mFilteredAccelX);
+		double dim = 255.0 / ACC_GYRO_MAX_VALUE;
 		int res = (int)(mFilteredAccelX * dim) % 255;
 		mNeoPixel.setPixelColor(i, mNeoPixel.Color(res, res, res));
 #elif (NEOPIXEL_STYLE_3 == 1)
 #define STYLE_3_AMOUNT	10
+		mFilteredAccelX = fabs(mFilteredAccelX);
 		if (mColorDir) {
 			mColorValue += STYLE_3_AMOUNT;
 			if (mColorValue > (uint32_t)0xFFFFFF) {
@@ -119,27 +108,73 @@ void loop() {
 		uint8_t blue = (uint8_t)(mColorValue & 0xFF);
 		mNeoPixel.setPixelColor(i, mNeoPixel.Color(red, green, blue, val));
 #elif (NEOPIXEL_STYLE_4 == 1)
+		double threshold = 10.0;
+		mFilteredAccelX = fabs(mFilteredAccelX);
 		int val = (int)pow((int)mFilteredAccelX % ACC_GYRO_MAX_VALUE, 2);
-
-		if (mColorDir) {
-			mColorValue += val;
-			if (mColorValue > (uint32_t)0xFFFFFF) {
-				mColorValue = (uint32_t)0xFFFFFF;
-				mColorDir = false;
-			}
-	}
+		uint8_t red, green, blue;
+		if (mFilteredAccelX < threshold) {
+			red = 255;
+			green = 255;
+			blue = 255;
+		}
 		else {
-			mColorValue -= val;
-			if (mColorValue < (uint32_t)0x000000) {
-				mColorValue = (uint32_t)0;
-				mColorDir = true;
+			if (mColorDir) {
+				mColorValue += val;
+				if (mColorValue > (uint32_t)0xFFFFFF) {
+					mColorValue = (uint32_t)0xFFFFFF;
+					mColorDir = false;
+				}
+			}
+			else {
+				mColorValue -= val;
+				if (mColorValue < (uint32_t)0x000000) {
+					mColorValue = (uint32_t)0;
+					mColorDir = true;
+				}
+			}
+			red = (uint8_t)((mColorValue >> 16) & 0xFF);
+			green = (uint8_t)((mColorValue >> 8) & 0xFF);
+			blue = (uint8_t)(mColorValue & 0xFF);
+		}
+		
+		mNeoPixel.setPixelColor(i, mNeoPixel.Color(red, green, blue));
+#elif (NEOPIXEL_STYLE_5 == 1)
+		double threshold = 10.0;
+		double _max = 255.0;
+		uint8_t red, green, blue;
+		if (-threshold < mFilteredAccelX && mFilteredAccelX < threshold) {
+			red = (uint8_t)_max;
+			green = (uint8_t)_max;
+			blue = (uint8_t)_max;
+		}
+		else if (mFilteredAccelX >= threshold) {
+			red = (uint8_t)_max;
+			green = (uint8_t)(_max - (_max / (double)ACC_GYRO_MAX_VALUE * mFilteredAccelX)); 
+			blue = (uint8_t)(_max - (_max / (double)ACC_GYRO_MAX_VALUE * mFilteredAccelX));
+		}
+		else if (mFilteredAccelX <= -threshold) {
+			blue = (uint8_t)_max;
+			green = (uint8_t)(_max + (_max / (double)ACC_GYRO_MAX_VALUE * mFilteredAccelX));
+			red = (uint8_t)(_max + (_max / (double)ACC_GYRO_MAX_VALUE * mFilteredAccelX));
+		}
+		mNeoPixel.setPixelColor(i, mNeoPixel.Color(red, green, blue));
+#elif (NEOPIXEL_STYLE_6 == 1)
+		double _max = 255.0;
+		uint8_t val;
+		if (mFilteredAccelX > 0) {
+			if (i < NEOPIXEL_NUM / 2) {
+				val = (uint8_t)(_max / (double)ACC_GYRO_MAX_VALUE * mFilteredAccelX);
+				mNeoPixel.setPixelColor(i, mNeoPixel.Color(val, val, val));
+			}
+			
+		}
+		else {
+			if (i >= NEOPIXEL_NUM / 2) {
+				val = (uint8_t)(_max / (double)ACC_GYRO_MAX_VALUE * -mFilteredAccelX);
+				mNeoPixel.setPixelColor(i, mNeoPixel.Color(val, val, val));
 			}
 		}
-
-		uint8_t red = (uint8_t)((mColorValue >> 16) & 0xFF);
-		uint8_t green = (uint8_t)((mColorValue >> 8) & 0xFF);
-		uint8_t blue = (uint8_t)(mColorValue & 0xFF);
-		mNeoPixel.setPixelColor(i, mNeoPixel.Color(red, green, blue));
+		
 #endif //end NEOPIXEL_STYLE_N
 		mNeoPixel.show();
 	}
--- SkateBoard/SkateBoard/defconfig.h
+++ SkateBoard/SkateBoard/defconfig.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#define ACC_GYRO_SENSOR
-
 #define ACC_GYRO_MAX_VALUE	90
 
 #define NEOPIXEL_STYLE_1	(0)
 #define NEOPIXEL_STYLE_2	(0)
 #define NEOPIXEL_STYLE_3	(0)
-#define NEOPIXEL_STYLE_4	(1)
+#define NEOPIXEL_STYLE_4	(0)
+#define NEOPIXEL_STYLE_5	(0)
+#define NEOPIXEL_STYLE_6	(1)
